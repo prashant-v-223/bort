939c2092cd0ed279c9e50bf9ee05758cedd2c93f
prashant-v-223 <109692632+prashant-v-223@users.noreply.github.com>
2024-08-01 20:05:20 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,53 @@
 // pages/_app.js
+import Layout from '@/componets/Layout';
 import '../styles/Home.module.css';
 import '../styles/globals.css';
 import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
 import { ToastProvider } from 'react-toast-notifications';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+ const [products1, setProducts1] = useState({ pixelId: "" });
 
+  useEffect(() => {
+    fetchProducts1();
+  }, []);
+  const fetchProducts1 = async () => {
+    try {
+      let headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${localStorage.getItem("token")}`
+      };
+
+      const response = await fetch('/api/upichange', {
+        method: 'GET',
+        headers: headersList,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("upi", data.upi.upi)
+        setProducts1(data.pixelId);
+      } else {
+      }
+    } catch (error) {
+    }
+
+  };
   return (
+      <Layout data={products1}>
     <ToastProvider>
       <Component {...pageProps} />
       <ToastContainer />
     </ToastProvider>
+      </Layout>
   );
 }
 
